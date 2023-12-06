b71e536ca2346ea6cca8e4aeecde2e5babfb54ec
mpiccoli18 <152176404+mpiccoli18@users.noreply.github.com>
2023-12-06 19:25:41 +0100

    Add files via upload
    Codice HTML della pagina home con header, footer, qualcosa del body e con i loghi dei social

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="keywords" content="Festival, Techno, Shop, Prevendite, Concerti, Lum250, Artisti, Merch">
+        <meta name="description" content="Pagina dedicata al festival Techno Lum250">
+        <title>TechnoLum Festival</title>
+        <link rel="icon" href="logofavicon.png" type="image/png">
+    </head>
+    <body>
+        <header>
+            <ul>
+                <img src="logo2.png" alt="TechnoLum Festival">
+                <h1>TechnoLum Festival</h1>
+                <a href="#" alt="Facebook logo"><img src="facebook2.png"></a>
+                <a href="#" alt="Spotify Logo"><img src="spotify2.png"></a>
+                <a href="#" alt="Instagram Logo"><img src="instagram2.png"></a>
+                <a href="#" alt="Twitter Logo"><img src="twitter2.png"></a>
+            </ul>
+        </header>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Festival
+                    <ul>
+                        <li><a href="chisiamo.php">Chi siamo</a></li>
+                        <li><a href="location.php">Location</a></li>
+                        <li><a href="storia.php">Storia del Festival</a></li>
+                    </ul>                    
+                </li>
+                <li>Shop
+                    <ul>
+                        <li><a href="merch.php">Merch</a></li>
+                        <li><a href="merch.php">Prevendite</a></li>
+                    </ul>
+                </li>
+                <li><a href="faq.php">FAQ</a></li>
+                <li><a href="account.php">Account</a></li>
+            </ul>
+        </nav>
+        <footer>
+            <ul>
+                <a href="#" alt="Facebook logo"><img src="facebook2.png"></a>
+                <a href="#" alt="Spotify Logo"><img src="spotify2.png"></a>
+                <a href="#" alt="Instagram Logo"><img src="instagram2.png"></a>
+                <a href="#" alt="Twitter Logo"><img src="twitter2.png"></a>
+            </ul><br>
+            <p>
+                <ul>
+                    <a href="#" alt="termini e condizioni">Termini e Condizioni</a>
+                    <a href="#" alt="privacy policy">Privacy Policy</a>
+                </ul>
+                TELE RADIO CITY S.C.S. -- P.IVA 00994500288
+            </p>
+        </footer>
+    </body>
+
+</html>
