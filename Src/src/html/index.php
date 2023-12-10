b06b071e09e8f5a30a4b8047fe0a7570133d2e51
Matteo Piccoli <mpiccoli18@gmail.com>
2023-12-10 10:50:37 +0100

    Modificate le sezioni header e footer nell'index, con aggiunta di funzionalità nel css. Modificati anche i loghi social tramite css.

---
+++ Src/src/html/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="keywords" content="Festival, Techno, Shop, Prevendite, Concerti, Lum250, Artisti, Merch">
+        <meta name="description" content="Pagina dedicata al festival Techno Lum250">
+        <title>TechnoLum Festival</title>
+        <link rel="icon" href="logo_nuovo_favicon.png" type="image/png">
+        <link rel="stylesheet" href="csswebsite.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    <body>
+        <header>
+            <nav id="menù" role="navigazione" aria-label="navigazione menù">
+                <ul>
+                    <li lang="en">Home</li>
+                    <li id="sottomenù" lang="en" aria-haspopup="true" aria-expanded="false">Festival
+                        <ul class="sottomenù">
+                            <li><a href="chisiamo.html" tabindex="1">Chi siamo</a></li>
+                            <li><a href="location.html" lang="en" tabindex="2">Location</a></li>
+                            <li><a href="storia.html" tabindex="3">Storia del <span lang="en">Festival</span></a></li>
+                        </ul>                    
+                    </li>
+                    <li id="sottomenù2" lang="en" aria-haspopup="true" aria-expanded="false" tabindex="0">Shop
+                        <ul class="sottomenù2">
+                            <li><a href="merch.html" lang="en"tabindex="4">Merch</a></li>
+                            <li><a href="biglietti.html" tabindex="5">Prevendite</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="faq.html" lang="en" tabindex="6"><abbr title="Frequently Asked Questions">FAQ</abbr></a></li>
+                    <li><a href="account.html" lang="en" tabindex="7">Account</a></li>
+                </ul>
+            </nav>
+            <a href="index.php"><img src="logo_nuovo_2.png" alt="TechnoLum Festival"></a>
+            <div id="social-icon">
+                <div id="facebook-logo"><a href="#"><i class="fa fa-facebook fa-2x" aria-hidden="true" alt="Facebook Logo"></i></a></div>
+                <div id="facebook-logo"><a href="#"><i class="fa fa-instagram fa-2x" aria-hidden="true" alt="Instagram Logo"></i></a></div>
+                <div id="facebook-logo"><a href="#"><i class="fa fa-spotify fa-2x" aria-hidden="true" alt="Spotify Logo"></i></a></div>
+                <div id="facebook-logo"><a href="#"><i class="fa fa-twitter fa-2x" aria-hidden="true" alt="Twitter Logo"></i></a></div>
+            </div>
+        </header>
+        <nav id="breadcrumb" role="sezione corrente" aria-label="Ti trovi nella sezione:">
+            <p>Ti trovi in: <span lang="en">Home</span></p>
+        </nav>            
+
+       <footer>
+            <div id="social-icon">
+                <div id="facebook-logo"><a href="#"><i class="fa fa-facebook fa-2x" aria-hidden="true" alt="Facebook Logo"></i></a></div>
+                <div id="facebook-logo"><a href="#"><i class="fa fa-instagram fa-2x" aria-hidden="true" alt="Instagram Logo"></i></a></div>
+                <div id="facebook-logo"><a href="#"><i class="fa fa-spotify fa-2x" aria-hidden="true" alt="Spotify Logo"></i></a></div>
+                <div id="facebook-logo"><a href="#"><i class="fa fa-twitter fa-2x" aria-hidden="true" alt="Twitter Logo"></i></a></div>
+            </div>
+            <a href="#" alt="termini e condizioni">Termini e Condizioni</a>
+            <a href="#" alt="privacy policy">Privacy Policy</a>
+            <p>
+                TELE RADIO <span lang="en">CITY</span> -- P.IVA 0099455028X
+            </p>
+        </footer>
+    </body>
+</html>
+
