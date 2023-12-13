e8bf2e168d01ec43150fb2ee86bd355edaa78283
Matteo Piccoli <mpiccoli18@gmail.com>
2023-12-13 17:49:46 +0100

    Aggiunte delle transizioni al logo e nel menù tramite CSS e JS, modificata la visualizzazione del menù

---
+++ Src/src/function.js
@@ -0,0 +1,8 @@
+function showMenu()
+{
+    document.getElementById("menu-header").classList.replace("is-invisible","is-visible");
+}
+function hideMenu()
+{
+    document.getElementById("menu-header").classList.replace("is-visible","is-invisible"); 
+}
---
+++ Src/src/index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="keywords" content="Festival, Techno, Shop, Prevendite, Concerti, Lum250, Artisti, Merch">
+        <meta name="description" content="Pagina dedicata al festival Techno Lum250">
+        <title>TechnoLum Festival</title>
+        <link rel="icon" href="logo_nuovo_favicon.png" type="image/png">
+        <link rel="stylesheet" href="csswebsite.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    <body>
+        <script src="function.js" type="text/javascript"></script>
+        <!-- QUESTA è LA PARTE DEL HEADER DEL FESTIVAL-->
+        <header>
+            <div id="hamburger-head" class="hamburger" onmouseleave="hideMenu()">
+                <i class="fa fa-solid fa-bars fa-4x hamburger-fa " onmouseenter="showMenu()"></i>                
+                <nav id="menu-header" class="menù is-invisible" role="navigazione" aria-label="navigazione menù" >
+                    <ul class="menù-ul">
+                        <li class="li-hover" lang="en">Home</li>
+                        <li class="sottomenù-li " lang="en" aria-haspopup="true" aria-expanded="false">Festival
+                            <ul class="sottomenù-ul">
+                                <li class="li-sottomenu li-hover"><a href="chisiamo.html" tabindex="1" >Chi siamo</a></li>
+                                <li class="li-sottomenu li-hover"><a href="location.html" lang="en" tabindex="2">Location</a></li>
+                                <li class="li-sottomenu li-hover "><a href="storia.html" tabindex="3">Storia del <span lang="en">Festival</span></a></li>
+                            </ul>                    
+                        </li>
+                        <li class="sottomenù2-li" lang="en" aria-haspopup="true" aria-expanded="false" tabindex="0">Shop
+                            <ul class="sottomenù2-ul">
+                                <li class="li-sottomenu li-hover"><a href="merch.html" lang="en" tabindex="4"><abbr title="Merchandise">Merch</abbr></a></li>
+                                <li class="li-sottomenu li-hover"><a href="biglietti.html" tabindex="5">Prevendite</a></li>
+                            </ul>
+                        </li>
+                        <li class="li-hover"><a href="faq.html" lang="en" tabindex="6"><abbr title="Frequently Asked Questions">FAQ</abbr></a></li>
+                        <li class="li-hover"><a href="account.html" lang="en" tabindex="7">Account</a></li>
+                    </ul>
+                </nav>
+            </div>                           
+            <a class="logo" href="index.php"><img id="img-logo" src="logo_nuovo_2.png" alt="TechnoLum Festival" class="LogoInnerImage" ></a>
+            <div class="social-icon-header">
+                <div class="facebook-logo"><a href="#"><i class="fa fa-facebook fa-2x" aria-hidden="true" alt="Facebook Logo"></i></a></div>
+                <div class="instagram-logo"><a href="#"><i class="fa fa-instagram fa-2x" aria-hidden="true" alt="Instagram Logo"></i></a></div>
+                <div class="spotify-logo"><a href="#"><i class="fa fa-spotify fa-2x" aria-hidden="true" alt="Spotify Logo"></i></a></div>
+                <div class="twitter-logo"><a href="#"><i class="fa fa-twitter fa-2x" aria-hidden="true" alt="Twitter Logo"></i></a></div>
+            </div>
+        </header>
+        <nav class="breadcrumb" role="sezione corrente" aria-label="Ti trovi nella sezione:">
+            <p>Ti trovi in: <span lang="en">Home</span></p>
+        </nav>
+        <!-- QUESTA è LA PARTE DEL PROGRAMMA DEL FESTIVA L-->
+        <div class="programma">
+            <h1>Il programma del festival è il seguente</h1>
+            <h2>Questo è un h2 invece!</h2>
+            <h3>Guarda la gallery per saperne di più!</h3>
+        </div>
+        <!-- QUESTA è LA PARTE DEL FOOTER DEL FESTIVAL-->
+        <footer>
+            <div class="social-icon-footer">
+                <div class="facebook-logo"><a href="#"><i class="fa fa-facebook fa-2x" aria-hidden="true" alt="Facebook Logo"></i></a></div>
+                <div class="instagram-logo"><a href="#"><i class="fa fa-instagram fa-2x" aria-hidden="true" alt="Instagram Logo"></i></a></div>
+                <div class="spotify-logo"><a href="#"><i class="fa fa-spotify fa-2x" aria-hidden="true" alt="Spotify Logo"></i></a></div>
+                <div class="twitter-logo"><a href="#"><i class="fa fa-twitter fa-2x" aria-hidden="true" alt="Twitter Logo"></i></a></div>
+            </div>
+            <div class="termini-privacy">
+                <a href="#" alt="termini e condizioni">Termini e Condizioni</a><br>
+                <a href="#" alt="privacy policy">Privacy Policy</a>
+            </div>
+            <p>
+                TELE RADIO <span lang="en">CITY</span> -- P.IVA 0099455028X
+            </p>
+        </footer>
+    </body>
+</html>
+
