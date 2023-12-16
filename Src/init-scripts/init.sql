09279dfaf71e3a3b5b303366a9871c1ba0d9cf0c
Dovid <giacomodovidio@gmail.com>
2023-12-16 19:43:59 +0100

    fix

--- Src/init-scripts/init.sql
+++ Src/init-scripts/init.sql
@@ -5,7 +5,7 @@ USE dbtecweb;
 CREATE TABLE IF NOT EXISTS users (
     id INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(50) NOT NULL,
-    password VARCHAR(50) NOT NULL
+    password VARCHAR(255) NOT NULL
 );
 
 INSERT INTO users (username, password) VALUES
