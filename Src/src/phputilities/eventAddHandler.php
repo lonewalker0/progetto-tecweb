d7ddd62d647a21130f56c9f8a3313a2806f12fa7
lonewalker0 <riccardoalberto.costantin@studenti.unipd.it>
2023-12-19 13:02:33 +0100

    ampliato docker compose per gestire i permessi sulla cartella, aggiunte 4 estensioni per i file immagine da uploadare

--- Src/src/phputilities/eventAddHandler.php
+++ Src/src/phputilities/eventAddHandler.php
@@ -11,17 +11,28 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['add_event'])) {
     // Gestisci il caricamento dell'immagine
     $nomeFileImmagine = $_FILES['image']['name'];
     $percorsoTemporaneoImmagine = $_FILES['image']['tmp_name'];
-    $percorsoCaricamentoImmagine = "../assets/artisti/" . $artistName . ".jpg";
-    #da sistemare perchè fnzioni con tutte le immagini non solo jpg
+    $percorsoCaricamentoImmagine = "../assets/artisti/" . $artistName . "." . pathinfo($nomeFileImmagine, PATHINFO_EXTENSION);
 
-    if (move_uploaded_file($percorsoTemporaneoImmagine, $percorsoCaricamentoImmagine)) {
-        if ($dboperation->addEvent($artistName, $date, $hour, $percorsoCaricamentoImmagine, $description)) {
-            header("Location: ../account.php"); 
-            die();
+    // Check if the file type is allowed
+    $allowedExtensions = ['jpg', 'jpeg', 'png', 'svg'];
+    $fileExtension = strtolower(pathinfo($nomeFileImmagine, PATHINFO_EXTENSION));
+
+    if (in_array($fileExtension, $allowedExtensions)) {
+        if (move_uploaded_file($percorsoTemporaneoImmagine, $percorsoCaricamentoImmagine)) {
+            if ($dboperation->addEvent($artistName, $date, $hour, $percorsoCaricamentoImmagine, $description)) {
+                header("Location: ../account.php"); 
+                die();
+            } else {
+                // Display an error message or redirect to an error page
+                echo "Error adding event. Please try again.";
+            }
         } else {
-            // Display an error message or redirect to an error page
-            echo "Error adding event. Please try again.";
+            // Display an error message for file upload failure
+            echo "Error uploading file. Please try again.";
         }
+    } else {
+        // Display an error message for disallowed file type
+        echo "Invalid file type. Please upload a JPG, JPEG, PNG, or SVG file.";
     }
 }
 ?>
