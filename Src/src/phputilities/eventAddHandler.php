e9642d60ae66a2005aa82372f91177363be7a44a
Dovid <giacomodovidio@gmail.com>
2023-12-23 00:28:25 +0100

    sistemata salvataggio immagini, nel server non funzionava

--- Src/src/phputilities/eventAddHandler.php
+++ Src/src/phputilities/eventAddHandler.php
@@ -12,6 +12,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['add_event'])) {
     $nomeFileImmagine = $_FILES['image']['name'];
     $percorsoTemporaneoImmagine = $_FILES['image']['tmp_name'];
     $percorsoCaricamentoImmagine = "../assets/artisti/" . $artistName . "." . pathinfo($nomeFileImmagine, PATHINFO_EXTENSION);
+    $percorsoImmagineDB = "assets/artisti/" . $artistName . "." . pathinfo($nomeFileImmagine, PATHINFO_EXTENSION);
 
     // Check if the file type is allowed
     $allowedExtensions = ['jpg', 'jpeg', 'png', 'svg'];
@@ -19,7 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['add_event'])) {
 
     if (in_array($fileExtension, $allowedExtensions)) {
         if (move_uploaded_file($percorsoTemporaneoImmagine, $percorsoCaricamentoImmagine)) {
-            if ($dboperation->addEvent($artistName, $date, $hour, $percorsoCaricamentoImmagine, $description)) {
+            if ($dboperation->addEvent($artistName, $date, $hour, $percorsoImmagineDB, $description)) {
                 header("Location: ../account.php"); 
                 die();
             } else {
