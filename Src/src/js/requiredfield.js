6f0b4b3145d3e47201422aa5166899ef86931480
Dovid <giacomodovidio@gmail.com>
2023-12-24 00:35:23 +0100

    ne serve un altro evidentemente

--- Src/src/js/requiredfield.js
+++ Src/src/js/requiredfield.js
@@ -1,12 +1,12 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var newPasswordField = document.getElementById('nuova_password');
-    var confirmPasswordField = document.getElementById('conferma_password2');
-    var oldPasswordField = document.getElementById('vecchia_password');
+document.addEventListener("DOMContentLoaded", function () {
+  var newPasswordField = document.getElementById("nuova_password");
+  var confirmPasswordField = document.getElementById("conferma_password2");
+  var oldPasswordField = document.getElementById("vecchia_password");
 
-    // Aggiungi un gestore agli eventi per il campo di input della nuova password
-    newPasswordField.addEventListener('input', function () {
-        // Rendi i campi obbligatori solo se la nuova password ha un valore
-        confirmPasswordField.required = this.value.trim() !== '';
-        oldPasswordField.required = this.value.trim() !== '';
-    });
+  // Aggiungi un gestore agli eventi per il campo di input della nuova password
+  newPasswordField.addEventListener("input", function () {
+    // Rendi i campi obbligatori solo se la nuova password ha un valore
+    confirmPasswordField.required = this.value.trim() !== "";
+    oldPasswordField.required = this.value.trim() !== "";
+  });
 });
