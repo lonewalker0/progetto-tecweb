74581343113e32499bb201773b0674f8f8a588f3
ipseunipd <davide.malgarise@studenti.unipd.it>
2023-12-30 17:00:17 +0100

    inizio pagina chi siamo + css

--- Src/src/chisiamo.php
+++ Src/src/chisiamo.php
@@ -7,7 +7,29 @@ $title = 'Chi siamo | TechnoLum250';
 $keyword = 'Festival, Techno, Lum250, Informazioni, Organizzatori;'; 
 $description = 'Vieni a conoscere coloro che hanno reso possibile il festival TechnoLum250, organizzatori, collaboratori e volontari.'; 
 
-$main = "<h1>Chi siamo</h1>";
+$main = "
+<div class='chi-siamo'>
+
+<h1>Chi siamo</h1>
+
+<img src='assets/festival.png' id='img-chi-siamo' alt='Evento festival'/>
+
+<p id='testo-chi-siamo'>TechnoLum250 Festival è un evento che riunisce centinaia di ragazzi nel cuore del centro universitario di Padova.
+    Tutto nasce dal desiderio degli studenti di salutare il periodo estivo degli esami per lasciarsi trasportare nel vivo dell'estate.
+    Questo è reso possibile grazie alla passione e alla collaborazione di molti ragazzi attivi in associazioni culturali, sportive e di volontariato sociale.
+</p>
+
+<p class='ringraziamenti'>I nostri ringraziamenti vanno a:</p>
+    <ul>
+        <li>Università degli Studi di Padova</li>
+        <li>Comune di Padova</li>
+        <li>The Big Bang Pizza</li>
+        <li>Libreria Progetto</li>
+        <li>Porta Portello</li>
+        <li>Bibione spiaggia</li>
+    </ul>
+</div>
+";
 
 echo PageBuilder::buildPage($breadcrumb, $breadcrumblen, $title ,$keyword, $description, $main);
 ?>
--- Src/src/domande.php
+++ Src/src/domande.php
@@ -16,21 +16,21 @@ $main = "
     <h2 class='faq-title'>Informazioni generali sul festival</h2>
     <div class='faq-box-container'>
     <div class='faq-item'>
-        <p class='question'>Qual'è l'età minima per poter partecipare a TechnoLum250 Festival?</p>
-        <p class='answer'>L'età minima per accedere al festival è 16. Le persone nate nell'anno 2008 avranno l'entrata garantita, anche se compiono i 16 anni dopo lo svolgimento dell'evento. Chiunque compia i 16 anni nel 2024 sarà ammesso.</p>
-        <p class='answer note'>Chiunque nato nell'anno 2009 o successivo, anche se accompagnato da persona adulta, non potrà accedere al festival.</p>  
+        <p class='question'>Qual è l'età minima per poter partecipare a TechnoLum250 Festival?</p>
+        <p class='answer'>L'entrata è garantita alle persone che hanno almeno 16 anni compiuti. Di conseguenza, chiunque nato nell'anno 2008 ma avente ancora 15 anni nella data corrispondente all'inizio dell'evento, non potrà accedere al festival.</p>
+        <p class='answer note'>L'accompagnamento da parte di una persona adulta non varia quanto scritto precedentemente.</p>  
     </div>
 
     <div class='faq-item'>
-        <p class='question'>Qual'è la <span lang='en'>line-up</span>?</p>
-        <p class='answer'>Tieni d'occhio i nostri canali social per essere il primo a sapere quando arriveranno nuovi nomi al TechnoLum250 Festival.</p>
-        <p class='answer note'>Puoi scoprire la <span lang='en'>line-up </span>nella pagina <a href='index.php'>home<a/>.</p>
+        <p class='question'>Qual è la <span lang='en'>line-up</span>?</p>
+        <p class='answer'>Tieni d'occhio i nostri canali <span lang='en'>social </span>per essere il primo a sapere quando arriveranno nuovi nomi al TechnoLum250 Festival.</p>
+        <p class='answer note'>Puoi scoprire la <span lang='en'>line-up </span>nella pagina <a href='index.php'>home</a>.</p>
     </div>
 
     <div class='faq-item'>
         <p class='question'>Quali oggetti sono vietati all'interno di TechnoLum250 festival?</p>
         <p class='answer'>Non è ammesso entrare nel territorio del festival con i seguenti oggetti:</p>
-        <ul>
+        <ul class='answer'>
             <li>armi da fuoco, armi bianche e oggetti appuntiti</li>
             <li>droga (eccetto per medicinali accompagnati da prescrizione medica)</li>
             <li>bottiglie o contenitori di vetro</li>
@@ -38,7 +38,7 @@ $main = "
             <li>cibo e bevande non venduti nei punti vendita da noi disposti</li>
             <li>fuochi d'artificio</li>
             <li>droni</li>
-            <li>spray</li>
+            <li><span lang='en'>spray</span></li>
             <li>bandiere UDU Padova o appartenenti a comunità LGBTQ+S</li>
             <li>qualsiasi altro oggetto che può essere considerato pericoloso dall'organizzazione</li>
         </ul>
@@ -53,23 +53,42 @@ $main = "
     <div class='faq-item'>
         <p class='question'>Dove posso acquistare i biglietti per TechnoLum250 Festival 2024?</p>
         <p class='answer'>Registrati presso la nostra pagina <span lang='en'>Account</span> per avere accesso alla vendita dei biglietti.
-        Maggiori informazioni riguardo la registrazione e la gestione dei propri acquisti possono essere trovati nella pagina<a href='account.php'>account<a/>.</p>
+        Maggiori informazioni riguardo la registrazione e la gestione dei propri acquisti possono essere trovati nella pagina <a href='account.php'>account</a>.</p>
         <p class='answer note'>Importante: la registrazione non garantisce l'acquisto dei biglietti, che saranno disponibili in quantità limitata.</p>
     </div>
 
     <div class='faq-item'>
-        <p class='question'>Come posso acquistare il merch dedicato?</p>
+        <p class='question'>Come posso acquistare il <span lang='en'>merch </span>dedicato?</p>
         <p class='answer'>Puoi acquistare i nostri articoli presso i punti vendita presenti in loco al festival.</p>
     </div>
 
     <div class='faq-item'>
-        <p class='question'>Posso chiedere il rimborso, un cambio o rivendere i miei ticket?</p>
-        <p class='answer'>In linea generale la nostra policy non prevede il rimborso o il cambio dei ticket acquistati. Ad ogni modo in specifici casi (per esempio in relazione ad una cancellazione del festival per cause di forza maggiore),
+        <p class='question'>Posso chiedere il rimborso, un cambio o rivendere i miei <span lang='en'>ticket</span>?</p>
+        <p class='answer'>In linea generale la nostra <span lang='en'>policy</span> non prevede il rimborso o il cambio dei <span lang='en'>ticket</span> acquistati. Ad ogni modo in specifici casi (per esempio in relazione ad una cancellazione del festival per cause di forza maggiore),
          allora il rimborso può essere previsto applicando una penale del 3% per spese amministrative. Non potrai più partecipare all'evento?
-         Il voucher elettronico che rappresenta il ticket non è nominale e per questo può essere trasferito senza una procedura particolare.</p>
+         Il <span lang='en'>voucher</span> elettronico che rappresenta il biglietto non è nominale e per questo può essere trasferito senza una procedura particolare.</p>
     </div>
     </div>
-</div>
+
+    <div class='faq-section'>
+        <h2 class='faq-title'>Info utili</h2>
+            <div class='faq-box-container'>
+                <div class='faq-item'>
+                    <p class='question'>Dove parcheggiare?</p>
+                    <p class='answer'</p>
+                </div>
+
+                <div class='faq-item'>
+                    <p class='question'>Apertura cancelli</p>
+                    <p class='answer'>Venerdì e sabato ore 19:00. Domenica ore 18:00.</p>
+                </div>
+
+                <div class='faq-item'>
+                    <p class='question'></p>
+                    <p class='answer'></p>
+                </div>
+            </div>
+    </div>
 </div>
 ";
 
