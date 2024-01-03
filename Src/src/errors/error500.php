c6fdbcd5ba3c85878b4d3f89908a10a3b98a20eb
lonewalker0 <riccardoalberto.costantin@studenti.unipd.it>
2024-01-03 18:35:22 +0100

    aggiunto pagina 404 e 500

---
+++ Src/src/errors/error500.php
@@ -0,0 +1,21 @@
+<?php
+include('../phputilities/PageBuilder.php');
+
+
+$breadcrumb = 'Server Error';
+$breadcrumblen = 'it'; 
+$title = 'Server Error | TechnoLum250';
+$keyword = 'Festival, Techno, Lum250, 500, errore, errore interno server;'; 
+$description = 'Pagina non trovata'; 
+
+$main = "
+<img src='../assets/error500.png' alt=''>
+
+            <h1>Errore Server</h1>
+                
+            <h2>Stiamo avendo dei problemi tecnici</h2>
+            <p>Ci scusiamo per il disagio.</p>";
+
+
+echo PageBuilder::buildPage($breadcrumb, $breadcrumblen, $title ,$keyword, $description, $main);
+?>
