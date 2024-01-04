d4fe22bd34cf67650e42a6ffb62d87a0ee017e8f
lonewalker0 <riccardoalberto.costantin@studenti.unipd.it>
2024-01-04 11:19:25 +0100

    sistemato location

--- Src/src/location.php
+++ Src/src/location.php
@@ -14,7 +14,7 @@ $main = "
 
         <div class='location-contenuto'>
 
-            <iframe id='map'src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3961.272301496815!2d11.883541114487894!3d45.40848241412682!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x477eda5872f993f3%3A0xf2694de3ecf7350e!2sAule%20Luzzatti!5e0!3m2!1sit!2sit!4v1704207798526!5m2!1sit!2sit'>
+            <iframe id='map' src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3961.272301496815!2d11.883541114487894!3d45.40848241412682!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x477eda5872f993f3%3A0xf2694de3ecf7350e!2sAule%20Luzzatti!5e0!3m2!1sit!2sit!4v1704207798526!5m2!1sit!2sit'>
             </iframe>
 
             <div class='testo-location'>
