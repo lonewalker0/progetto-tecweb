1b20e3621abacd728748ddf13686aedddc38f132
Matteo Piccoli <matteo.picc@outlook.it>
2024-01-04 13:03:54 +0100

    Fix header, menu e bread in tutte le pagine con il CSS

--- Src/src/js/functions.js
+++ Src/src/js/functions.js
@@ -36,7 +36,7 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 //se il path della pagina è progetto-git/index.php crea due eventListener per il carosello e il countdown
-if(window.location.pathname === '../index.php')
+if(window.location.pathname === '/progetto-git/index.php')
 {
     //eventListener per la visualizzazione del carosello e per la visualizzazione del countdown del festival
     document.addEventListener("DOMContentLoaded", function () {
@@ -130,7 +130,7 @@ function carosello() {
     }
   }
   caroselloj++;
-  setTimeout(carosello, 4000); //Cambia l'immagine del carosello ogni 2 secondi
+  setTimeout(carosello, 4000); //Cambia l'immagine del carosello ogni 4 secondi
 }
 
 function validateForm() {
