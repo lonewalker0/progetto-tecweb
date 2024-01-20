71c2b062d082cbe5f48c20432d065253ac35823f
Matteo Piccoli <mpiccoli18@gmail.com>
2024-01-20 19:24:30 +0100

    fix htaccess, aggiunta pagina errore 403

---
+++ Src/src/error403.php
@@ -0,0 +1,19 @@
+<?php
+include('phputilities/PageBuilder.php');
+
+
+$breadcrumb = 'Accesso ristretto';
+$breadcrumblen = 'it'; 
+$title = 'Accesso ristretto | TechnoLum250';
+$keyword = 'Festival, Techno, Lum250, 403, errore, accesso ristretto, risorsa;'; 
+$description = 'Accesso ristretto'; 
+
+$main = "
+<img src='../assets/error403.png' alt='403 Accesso ristretto'>
+
+<h1>Purtroppo non hai accesso a questa risorsa!</h1>
+    
+<h2>Verrai reinderizzato alla home tra pochi secondi...</h2>";
+
+echo PageBuilder::buildPage($breadcrumb, $breadcrumblen, $title ,$keyword, $description, $main);
+?>
--- Src/src/index.php
+++ Src/src/index.php
@@ -7,9 +7,7 @@ $title = 'Home | TechnoLum250';
 $keyword = 'Festival, Techno, Lum250, Programma, Concerti, Artisti, Cantanti, Shop'; 
 $description = 'Pagina dedicata al festival Techno Lum250, con il nostro programma per i prossimi eventi e artisti presenti.';
 
-// Create an instance of IndexMainBuilder
 $indexMainBuilder = new IndexMainBuilder();
-// Call the buildMainHTML method to get the generated HTML
 $main = $indexMainBuilder->buildMainHTML();
 
 echo PageBuilder::buildPage($breadcrumb, $breadcrumblen, $title ,$keyword, $description, $main);
